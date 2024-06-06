d1d6fc168e5af1b48a056a5ebd1a331dd2c8cd0a
Meisterschueler <kammermark@gmx.de>
2024-06-06 11:42:45 +0200

    Migrated phase1 step6: get WW15MGH.DAC

---
+++ phase1-ogn-software/06-get-WW15MGH/00-run-chroot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+HOME_DIR=/home/"${FIRST_USER_NAME}"
+
+# get WW15MGH.DAC for conversion between the Height-above-Elipsoid to Height-above-Geoid thus above MSL
+wget --no-check-certificate https://earth-info.nga.mil/GandG/wgs84/gravitymod/egm96/binary/WW15MGH.DAC -O "${HOME_DIR}"/WW15MGH.DAC
+chown 1000:1000 "${HOME_DIR}"/WW15MGH.DAC
+ln -s "${HOME_DIR}"/WW15MGH.DAC "${HOME_DIR}"/rtlsdr-ogn/WW15MGH.DAC
+
+### migration ###
+# - get the original file from internet instead of local file
+# - link the file into rtlsdr-ogn instead of copy
